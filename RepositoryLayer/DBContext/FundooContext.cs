792819611a8051f3594bc41366e053b6360f6dc2
navabhi22 <navabhi22@gmail.com>
2022-05-12 22:30:43 +0530

    [Abhinav] migration is done

---
+++ RepositoryLayer/DBContext/FundooContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.DBContext
+{
+    public class FundooContext:DbContext
+    {
+        public FundooContext(DbContextOptions options)
+           : base(options)
+        {
+
+        }
+
+        public DbSet<User> users { get; set; }
+    }
+}
---
+++ RepositoryLayer/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+    }
+}
